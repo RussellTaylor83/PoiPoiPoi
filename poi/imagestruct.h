7342a8b6aef98f47e0f6d13afdfbe1cfae629fed
Russell Taylor <rt@russtaylor.co.uk>
2015-11-24 19:46:09 +0000

    Attempted to put images into a structure
    Trying to keep durations, slices, and rotation speed with the image
    data.

---
+++ poi/imagestruct.h
@@ -0,0 +1,6 @@
+struct ImageStruct {
+  std::vector<unsigned int> image;
+  int duration;
+  int numberOfSlices;
+  unsigned long rotationSpeed;
+};
--- poi/poi.ino
+++ poi/poi.ino
@@ -1,24 +1,28 @@
-  #include <IRremote.h>
-#include "FastLED.h"
-#include "sequences.h"
+#include <IRremote.h>
+#include <FastLED.h>
+#include <vector>
 
-//#include "arrow.h"    //images
+#include "imagestruct.h" //has to be done like this to give the type the correct scope
+
+//#include "arrow.h"
 //#include "flower.h"
 //#include "skullxbones.h"
 //#include "justeat.h"
+//#include "infernopattern.h"
+//#include "lightning3blue.h"
+//#include "lightning3white.h"
+//#include "holdinghands.h"
 #include "celticknot.h"
-#include "infernopattern.h"
-#include "lightning3blue.h"
-#include "lightning3white.h"
-#include "holdinghands.h"
 
-#define NUM_BUTTONS 2   //number of remote buttons
-#define NUM_LEDS 60     //number of leds in strip length on one side
-#define DATA_PIN 2      //7 = second hardware spi data
-#define CLOCK_PIN 3     //14 = second hardware spi clock
-#define RECV_PIN 6
+#include "sequences.h"
 
-uint16_t lastCode = 0; //keeps track of last ir code
+#define NUM_BUTTONS   2       // number of remote buttons
+#define NUM_LEDS      60      // number of leds in strip length on one side
+#define DATA_PIN      2       // 7 = second hardware spi data
+#define CLOCK_PIN     3       // 14 = second hardware spi clock
+#define RECV_PIN      6
+
+uint16_t lastCode = 0;        // keeps track of last ir code
 boolean ledEnable = 1;
 
 const uint16_t BUTTON_ON = 0xFFE21D; //ir remote codes
@@ -29,7 +33,8 @@ IRrecv irrecv(RECV_PIN);
 
 decode_results results;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
   pinMode (RECV_PIN, INPUT);
@@ -46,21 +51,21 @@ void loop()
 {
   if (ledEnable)
   {
-   
-    
-    
-  //  PoiSonic(10000, 150, flower, 200); //call method, length of time image displayed, number of slices in image, image name, rotation speed
-  //  PoiSonic(10000, 150, arrow, 200);
-   // PoiSonic(10000, 77, skullxbones, 200);
-   // PoiSonic(20000, 231, justeat, 200);
-    PoiSonic(10000, 70, celticknot, 500);
-    PoiSonic(10000, 100, infernopattern, 500);
-    PoiSonic(10000, 150, lightning3white, 500);
-    PoiSonic(10000, 125, holdinghands, 500);
+    //    PoiSonic(10000, 150, flower, 200); //call method, length of time image displayed, number of slices in image, image name, rotation speed
+    //    PoiSonic(10000, 150, arrow, 200);
+    //    PoiSonic(10000, 77, skullxbones, 200);
+    //    PoiSonic(20000, 231, justeat, 200);
+    //    PoiSonic(10000, 70, celticknot, 500);
+    //PoiSonic(10000, 100, infernopattern, 500);
+    //PoiSonic(10000, 150, lightning3white, 500);
+    //PoiSonic(10000, 125, holdinghands, 500);
+
+    PoiSonic(&celticknot);
   }
 }
 
-void ResetStrips() {
+void ResetStrips()
+{
   FastLED.clear();
   FastLED.show();
 }
@@ -93,20 +98,17 @@ void CheckIR()
   }
 }
 
-void PoiSonic(unsigned long time, int numberOfSlices, const unsigned int array[], int rotationSpeed) {
+void PoiSonic(ImageStruct *image) {
   if (ledEnable == 0) {
     return;
   }
   unsigned long currentTime = millis();
-  while (millis() < currentTime + (time)) {
+  while (millis() < currentTime + (image->duration)) {
 
-    int f = numberOfSlices;
-    int z; //a counter
-    int j = NUM_LEDS;
-
-    for (int x = 0; x < f; x++) {
-      for (z = NUM_LEDS; z > 0; z--) {
-        leds[z - 1] = array[x + ((j - z) * f)];
+    int z; //just a count
+    for (int x = 0; x < image->numberOfSlices; x++) {                  // for each slice
+      for (z = NUM_LEDS; z > 0; z--) {                                // for each led
+        leds[z - 1] = image->image[x + ((NUM_LEDS - z) * image->numberOfSlices)];   //
         if (ledEnable == 0) {
           ResetStrips();
           return;
@@ -114,8 +116,7 @@ void PoiSonic(unsigned long time, int numberOfSlices, const unsigned int array[]
       }
       FastLED.show();
 
-      delayMicroseconds(rotationSpeed); //may need to increase / decrease depending on spin rate
+      delayMicroseconds(image->rotationSpeed); //may need to increase / decrease depending on spin rate
     }
   }
 }
-
