f1f9ede7626d1da584099cb6d81ee2dedb90ebc7
Russell Taylor <rt@russtaylor.co.uk>
2015-11-26 01:31:45 +0000

    Beginnings of sequence functionality
    Produces compile error when using more that two images, something wrong.

--- poi/poi.ino
+++ poi/poi.ino
@@ -3,17 +3,6 @@
 #include <vector>
 
 #include "imagestruct.h" //has to be done like this to give the type the correct scope
-
-//#include "arrow.h"
-//#include "flower.h"
-//#include "skullxbones.h"
-//#include "justeat.h"
-//#include "infernopattern.h"
-//#include "lightning3blue.h"
-//#include "lightning3white.h"
-//#include "holdinghands.h"
-#include "celticknot.h"
-
 #include "sequences.h"
 
 #define NUM_BUTTONS   2       // number of remote buttons
@@ -24,6 +13,8 @@
 
 uint16_t lastCode = 0;        // keeps track of last ir code
 boolean ledEnable = 1;
+int chosenSequence = 0;       
+int numberOfSequences = 0; //sizeof(Sequences);
 
 const uint16_t BUTTON_ON = 0xFFE21D; //ir remote codes
 const uint16_t BUTTON_OFF = 0xFFA25D;
@@ -51,18 +42,36 @@ void loop()
 {
   if (ledEnable)
   {
+    //for (unsigned int x = 0; x < sizeof(Sequences[chosenSequence]); x++) {      // for each image in the chosen sequence
+        //PoiSonic(&Sequences[chosenSequence][x]);
+    //}
+        
     //    PoiSonic(10000, 150, flower, 200); //call method, length of time image displayed, number of slices in image, image name, rotation speed
-    //    PoiSonic(10000, 150, arrow, 200);
+    //    PoiSonic(10000, 150, arrows, 200);
     //    PoiSonic(10000, 77, skullxbones, 200);
     //    PoiSonic(20000, 231, justeat, 200);
     //    PoiSonic(10000, 70, celticknot, 500);
-    //PoiSonic(10000, 100, infernopattern, 500);
-    //PoiSonic(10000, 150, lightning3white, 500);
-    //PoiSonic(10000, 125, holdinghands, 500);
-
-    PoiSonic(&celticknot);
+    //    PoiSonic(10000, 100, infernopattern, 500);
+    //    PoiSonic(10000, 150, lightning3white, 500);
+    //    PoiSonic(10000, 125, holdinghands, 500);
   }
 }
+
+//void SequenceUp() {
+//  if (chosenSequence == numberOfSequences) {
+//    chosenSequence = 0;
+//  } else {
+//    chosenSequence++;
+//  }
+//}
+//
+//void SequenceDown() {
+//  if (chosenSequence == 0) {
+//    chosenSequence = numberOfSequences;
+//  } else {
+//    chosenSequence--;
+//  }
+//}
 
 void ResetStrips()
 {
@@ -105,7 +114,8 @@ void PoiSonic(ImageStruct *image) {
   unsigned long currentTime = millis();
   while (millis() < currentTime + (image->duration)) {
 
-    int z; //just a count
+    int z; //just a count, should be able to move this into the second for loop? same as int x =
+    
     for (int x = 0; x < image->numberOfSlices; x++) {                  // for each slice
       for (z = NUM_LEDS; z > 0; z--) {                                // for each led
         leds[z - 1] = image->image[x + ((NUM_LEDS - z) * image->numberOfSlices)];   //
--- poi/sequences.h
+++ poi/sequences.h
@@ -1,9 +1,16 @@
-//#include "arrow.h"
-//#include "flower.h"
 //#include "skullxbones.h"
-//
-//int sequences[2];
-//
-//sequences[0] = 1;
-//sequences[1] = 3;
+//#include "justeat.h"
+//#include "infernopattern.h"
+//#include "lightning3blue.h"
+//#include "lightning3white.h"
+//#include "holdinghands.h"
 
+#include "celticknot.h"
+//#include "skullxbones.h"
+#include "arrows.h"
+//#include "flower.h"
+
+//ImageStruct Sequences[2][2] = {
+//  {celticknot, flower},
+//  {skullxbones, arrows }
+//};
